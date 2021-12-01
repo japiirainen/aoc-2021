bb02ccd0114675cb6f8ffaa40b2c15ce0de37ebb
Joona Piirainen <joona.piirainen@gmail.com>
2021-12-01 08:41:55 +0200

    rename increasing

--- src/Days/Day1.hs
+++ src/Days/Day1.hs
@@ -2,16 +2,16 @@ module Days.Day1 where
 
 import           Data.Foldable (foldl')
 
-increasing :: [Int] -> Int
-increasing = snd . foldl' (\(prev, count) x -> if x > prev then (x, count + 1) else (x, count)) (maxBound, 0)
+countIncreasing :: [Int] -> Int
+countIncreasing = snd . foldl' (\(prev, count) x -> if x > prev then (x, count + 1) else (x, count)) (maxBound, 0)
 
 part2 :: [Int] -> Int
-part2 = increasing . map sum . sliding 3
+part2 = countIncreasing . map sum . sliding 3
 
 main :: IO ()
 main = do
     numbers <- map read . lines <$> readFile "inputs/day1.txt" :: IO [Int]
-    let r1 = increasing numbers
+    let r1 = countIncreasing numbers
         r2 = part2 numbers
     print $ "Part 1 result: " <> show r1
     print $ "Part 2 result: " <> show r2

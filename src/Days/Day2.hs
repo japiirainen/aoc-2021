7507636a5efb758a001e42acd1308266e68d6d10
Joona Piirainen <joona.piirainen@gmail.com>
2021-12-02 16:54:31 +0200

    clean up day2

--- src/Days/Day2.hs
+++ src/Days/Day2.hs
@@ -1,43 +1,40 @@
-{-# LANGUAGE LambdaCase          #-}
-{-# LANGUAGE ScopedTypeVariables #-}
+{-# LANGUAGE LambdaCase #-}
 module Days.Day2 where
-import           Data.Foldable (foldl')
 
-data Direction
-    = Forward
-    | Down
-    | Up
+import           Control.Applicative
+import           Data.Foldable       (foldl')
+
+import qualified AOC.Parsija         as P
+
+data Command = Forward !Int | Down !Int | Up !Int
     deriving (Show)
 
-data Command = MkCommand Direction Int
-    deriving (Show)
-
-parseCommand :: String -> Command
-parseCommand s = case words s of
-    ["forward", n] -> MkCommand Forward (read n)
-    ["down", n]    -> MkCommand Down (read n)
-    ["up", n]      -> MkCommand Up (read n)
-    _              -> error "Invalid input"
+parseCommands :: P.Parser Char [Command]
+parseCommands = many $
+    (P.string "forward" *> P.spaces *> (Forward <$> P.decimal <* P.spaces)) <|>
+    (P.string "down"    *> P.spaces *> (Down    <$> P.decimal <* P.spaces)) <|>
+    (P.string "up"      *> P.spaces *> (Up      <$> P.decimal <* P.spaces))
 
 part1 :: [Command] -> Int
 part1 cs = let (hor, dep) = foldl' step (0, 0) cs in hor * dep
     where
         step = \(hor, dep) -> \case
-                MkCommand Forward n -> (hor + n, dep)
-                MkCommand Down n    -> (hor, dep + n)
-                MkCommand Up n      -> (hor, dep - n)
+                Forward n -> (hor + n, dep)
+                Down n    -> (hor, dep + n)
+                Up n      -> (hor, dep - n)
 
 part2 :: [Command] -> Int
 part2 cs = let (hor, dep, _) = foldl' step (0, 0, 0) cs in hor * dep
     where
         step = \(hor, dep, aim) -> \case
-                MkCommand Forward n -> (hor + n, dep + (aim * n), aim)
-                MkCommand Down n    -> (hor, dep, aim + n)
-                MkCommand Up n      -> (hor, dep, aim - n)
+                Forward n -> (hor + n, dep + (aim * n), aim)
+                Down n    -> (hor, dep, aim + n)
+                Up n      -> (hor, dep, aim - n)
 
 main :: IO ()
-main = do
-    commands <- map parseCommand . lines <$> readFile "inputs/day2.txt" :: IO [Command]
-    print $ "Part1: " <> show (part1 commands)
-    print $ "Part2: " <> show (part2 commands)
-    pure ()
+main = readFile "inputs/day2.txt" >>= \input -> do
+    case P.runParser parseCommands input of
+        Left e -> print e
+        Right commands -> do
+            print $ "Part 1: " <> show (part1 commands)
+            print $ "Part 2: " <> show (part2 commands)

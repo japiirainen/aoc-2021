718aec1b46acbcdb3327f349cd3950ba1e680d6e
Joona Piirainen <joona.piirainen@gmail.com>
2021-12-07 10:09:02 +0200

    clean up 07

--- haskell/2021/07.hs
+++ haskell/2021/07.hs
@@ -1,27 +1,28 @@
 {-# LANGUAGE TypeApplications #-}
+module Main where
 
 import qualified AOC.Parsija   as P
 import           Control.Arrow ((&&&))
 
+minMax :: [Int] -> (Int, Int)
+minMax = minimum &&& maximum
+
+solve :: (Int -> Int) -> [Int] -> Int
+solve trans ps = minimum [calculateCosts trans pos ps | let (minB, maxB) = minMax ps, pos <- [minB..maxB]]
+
+calculateCosts :: (Int -> Int) -> Int -> [Int] -> Int
+calculateCosts trans pos = sum . map (trans . abs . (pos -))
+
+incWithDist :: Int -> Int
+incWithDist x = x * (x + 1) `div` 2
 
 main :: IO ()
 main = do
     ip <- readFile "2021/07.txt"
-    case P.runParser (P.decimal @Int `P.sepBy` P.char ',') ip of
+    case P.runParser (P.decimal @Int `P.sepBy1` P.char ',') ip of
         Left err -> print err
-        Right positions -> do
-            print $ "Part 1: " <> show (solve id positions)
-            print $ "Part 2: " <> show (solve increaseWithDistance positions)
-
-
-increaseWithDistance :: Int -> Int
-increaseWithDistance x = x * (x + 1) `div` 2
-
-solve :: (Int -> Int) -> [Int] -> Int
-solve distToFuel positions = minimum [cost position distToFuel positions | let (minB, maxB) = minMax positions, position <- [minB .. maxB]]
-
-minMax :: [Int] -> (Int, Int)
-minMax = minimum &&& maximum
-
-cost :: Int -> (Int -> Int) -> [Int] -> Int
-cost position distToFuel = sum . map (distToFuel . abs . (position -))
+        Right xs -> do
+            let sol1 = solve id xs
+                sol2 = solve incWithDist xs
+            print $ "Part 1: " <> show sol1
+            print $ "Part 2: " <> show sol2

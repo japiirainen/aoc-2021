33ce2fed75f0e386999cdc9dc9e2912a363f1ab2
Joona Piirainen <joona.piirainen@gmail.com>
2021-12-09 12:11:21 +0200

    day9 in haskell

--- 2021/09.hs
+++ 2021/09.hs
@@ -1,25 +1,49 @@
+{-# LANGUAGE LambdaCase #-}
 module Main where
 
-import qualified AOC.Grid    as G
-import           AOC.Main    (pureMain)
+import           AOC.Main                (pureMain)
+import           Control.Monad           (guard)
+import           Data.Char               (isDigit)
+import           Data.List               (sortOn)
+import           Data.Maybe              (maybeToList)
+import           Data.Ord                (Down (..))
+import           Data.Text.Internal.Read (digitToInt)
+import           Data.Traversable        (for)
 
-import qualified AOC.Parsija as P
+import           AOC.Dijkstra            (Bfs (..), bfs)
+import qualified AOC.Grid                as G
+import qualified Data.Map                as M
 
-parseInput :: P.Parser Char (G.Grid Int)
-parseInput = G.fromList <$> parseBoardLine `P.sepBy1` P.newline
+lowPoints :: Ord a => G.Grid a -> [(G.Pos, a)]
+lowPoints grid = do
+    (pos, height) <- M.toList grid
+    guard . and $ do
+        n <- G.neighbors pos
+        h <- maybeToList $ M.lookup n grid
+        pure $ h > height
+    pure (pos, height)
+
+basin :: Ord a => G.Grid a -> G.Pos -> [G.Pos]
+basin grid = M.keys . bfsDistances . bfs neighbors (const False)
     where
-        parseBoardLine = read <$> P.many1 P.char "" <* P.horizontalSpaces
-
+        neighbors :: G.Pos -> [G.Pos]
+        neighbors pos = do
+            height <- maybeToList $ M.lookup pos grid
+            n <- G.neighbors pos
+            h <- maybeToList $ M.lookup n grid
+            guard $ h > height
+            pure n
 
 main :: IO ()
-main = do
-    file <- readFile "2021/09-sample.txt"
-    case P.runParser parseInput file of
-        Left err -> print err
-        Right grid -> do
-            print grid
--- main :: IO ()
--- main = pureMain $ \input -> do
---     ip <- P.runParser parseInput input
+main = pureMain $ \input -> do
+    grid <- for (G.fromString input) $ \case
+        c | isDigit c -> Right $ digitToInt c
+        c             -> Left $ "Unknown char " ++ show c
 
---     pure (pure (), pure ())
+    let lps = lowPoints grid
+        p1  = sum $ map (succ . snd) lps
+        grid' = M.filter (< 9) grid
+        basins = map (basin grid' . fst) lps
+        p2 = product . take 3 $ sortOn Down (map length basins)
+
+    pure (pure p1, pure p2)

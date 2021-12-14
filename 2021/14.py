697d9080c1cc2aad807ca4a43ae229fbea60f299
Joona Piirainen <joona.piirainen@gmail.com>
2021-12-14 12:39:02 +0200

    14: clean up

--- 2021/14.py
+++ 2021/14.py
@@ -1,7 +1,6 @@
 #!/usr/bin/python3
 from collections import Counter
 
-
 s, rules = open('14.txt').read().split('\n\n')
 
 r = {}
@@ -9,21 +8,20 @@ for line in rules.splitlines():
     start, end = line.split(' -> ')
     r[start] = end
 
-c1 = Counter()
-for i in range(len(s)-1):
-    c1[s[i]+s[i+1]] += 1
+c1 = Counter([s[i]+s[i+1] for i in range(len(s)-1)])
 
-for t in range(41):
-    if t in [10, 40]:
-        cf = Counter()
-        for k in c1:
-            cf[k[0]] += c1[k]
-        cf[s[-1]] += 1
-        print(max(cf.values())-min(cf.values()))
-
+for t in range(40):
     # If AB->R, then AB becomes (AR, RB)
     c2 = Counter()
     for k in c1:
         c2[k[0]+r[k]] += c1[k]
         c2[r[k]+k[1]] += c1[k]
     c1 = c2
+
+    if t in [9, 39]:
+        cf = Counter()
+        for k in c1:
+            cf[k[0]] += c1[k]
+        # Always inc the last char, since we only add in the middle
+        cf[s[-1]] += 1
+        print(max(cf.values())-min(cf.values()))

8b811f906c09091cbf91a1ae516a5367e99bb133
Joona Piirainen <joona.piirainen@gmail.com>
2021-12-16 16:38:13 +0200

    day 16

--- 2021/15.hs
+++ 2021/15.hs
@@ -1,55 +1,49 @@
 module Main where
 
 import qualified AOC.Dijkstra as Dijkstra
-import qualified AOC.Grid as G
+import qualified AOC.Grid.Bounded as G
 import AOC.Main (pureMain)
-import AOC.V2 (V2 (..), (.+.))
-import qualified AOC.V2.Box as Box
+import AOC.V2 (V2 (..))
 import Data.Char (digitToInt, isDigit)
-import qualified Data.Map as M
 import Data.Maybe (mapMaybe, maybeToList)
 import Data.Traversable (for)
 
 solve :: G.Grid Int -> Either String Int
-solve grid = do
-  (start, end) <- case G.box grid of
-    Just b -> pure (Box.bTopLeft b, Box.bBottomRight b)
-    Nothing -> Left "empty grid"
-  let dijkstra =
-        Dijkstra.dijkstra
-          ( \pos -> do
-              n <- G.neighbors pos
-              v <- maybeToList $ M.lookup n grid
-              pure (v, n)
-          )
-          (== end)
-          start
-  case Dijkstra.dijkstraGoal dijkstra of
-    Nothing -> Left "no path found"
-    Just (_, _, path) ->
-      pure . sum $ mapMaybe (`M.lookup` grid) (drop 1 $ reverse path)
+solve grid = case Dijkstra.dijkstraGoal dijkstra of
+  Nothing -> Left "no path found"
+  Just (_, _, path) ->
+    pure . sum $
+      mapMaybe (`G.lookup` grid) (drop 1 $ reverse path)
+  where
+    start = V2 0 0
+    end = V2 (G.gridWidth grid - 1) (G.gridHeight grid - 1)
+    dijkstra =
+      Dijkstra.dijkstra
+        ( \pos -> do
+            n <- G.neighbors pos
+            v <- maybeToList $ G.lookup n grid
+            pure (v, n)
+        )
+        (== end)
+        start
 
-extend :: Int -> G.Grid Int -> Either String (G.Grid Int)
-extend n grid = do
-  V2 gw gh <- case G.box grid of
-    Just b -> pure $ V2 (Box.width b) (Box.height b)
-    Nothing -> Left "empty grid"
-  pure $
-    M.fromList $ do
-      xg <- [0 .. n - 1]
-      yg <- [0 .. n - 1]
-      (pos, val) <- M.toList grid
-      let pos' = pos .+. V2 (xg * gw) (yg * gh)
-          val' = (val + xg + yg - 1) `mod` 9 + 1
-      pure (pos', val')
+extend :: Int -> G.Grid Int -> G.Grid Int
+extend n grid = G.generate (n * gw) (n * gh) $ \(V2 x y) ->
+  let (gx, x') = divMod x gw
+      (gy, y') = divMod y gh
+      val = G.index (V2 x' y') grid
+   in (val + gx + gy - 1) `mod` 9 + 1
+  where
+    gw = G.gridWidth grid
+    gh = G.gridHeight grid
 
 main :: IO ()
 main = pureMain $ \input -> do
-  grid <- for (G.fromString input) $ \c ->
+  cGrid <- G.fromString input
+  grid <- for cGrid $ \c ->
     if isDigit c
       then Right $ digitToInt c
       else Left $ "Invalid character: " ++ show c
   part1 <- solve grid
-  grid' <- extend 5 grid
-  part2 <- solve grid'
+  part2 <- solve $ extend 5 grid
   pure (pure part1, pure part2)

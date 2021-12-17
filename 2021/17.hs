fa6376a8e8ad6a732e2fb60e6dba47939c1218bd
Joona Piirainen <joona.piirainen@gmail.com>
2021-12-17 09:27:06 +0200

    parse day 17 input from file

--- 2021/17.hs
+++ 2021/17.hs
@@ -1,27 +1,36 @@
 module Main where
 
+import AOC.Main (pureMain)
+import qualified AOC.Parsija as P
 import Control.Monad (guard)
 
-xmin, xmax, ymin, ymax :: Int
-(xmin, xmax) = (281, 311)
-(ymin, ymax) = (-74, -54)
+type Bounds = ((Int, Int), (Int, Int))
 
-arc :: (Int, Int) -> [(Int, Int)]
-arc = takeWhile (\(_, y) -> y >= ymin) . steps (0, 0)
+parseInput :: P.Parser Char Bounds
+parseInput = (,) <$> px <*> py
+  where
+    px = (,) <$> (P.string "target area: x=" *> P.decimal) <*> (P.string ".." *> P.decimal <* P.string ",")
+    py = (,) <$> (P.spaces *> P.string "y=-" *> (negate <$> P.decimal)) <*> (P.string "..-" *> (negate <$> P.decimal))
+
+arc :: Bounds -> (Int, Int) -> [(Int, Int)]
+arc (_, (ymin, _)) = takeWhile (\(_, y) -> y >= ymin) . steps (0, 0)
   where
     steps (x, y) (dx, dy) =
       (x, y) : steps (x + dx, y + dy) (dx - signum dx, dy - 1)
 
-validArc :: [(Int, Int)] -> Bool
-validArc = any (\(x, y) -> x >= xmin && x <= xmax && y >= ymin && y <= ymax)
+validArc :: Bounds -> [(Int, Int)] -> Bool
+validArc ((xmin, xmax), (ymin, ymax)) =
+  any (\(x, y) -> x >= xmin && x <= xmax && y >= ymin && y <= ymax)
 
-as :: [[(Int, Int)]]
-as = do
+arcs :: Bounds -> [[(Int, Int)]]
+arcs bs@((_, xmax), (ymin, _)) = do
   dx <- [1 .. xmax]
   dy <- [ymin .. negate ymin]
-  let x = arc (dx, dy)
-  guard $ validArc x
+  let x = arc bs (dx, dy)
+  guard $ validArc bs x
   pure x
 
 main :: IO ()
-main = print (maximum . (concatMap . map) snd $ as) >> print (length as)
+main = pureMain $ \input -> do
+  bounds <- P.runParser parseInput input
+  pure (pure (maximum . (concatMap . map) snd $ arcs bounds), pure (length $ arcs bounds))

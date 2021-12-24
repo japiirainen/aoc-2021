e1691b95e9b32be3368da76817d4dc282f2d7746
Joona Piirainen <joona.piirainen@gmail.com>
2021-12-24 11:02:43 +0200

    refactor day 23

--- 2021/23.hs
+++ 2021/23.hs
@@ -2,82 +2,74 @@
 
 module Main where
 
-import AOC.Coord
-  ( Coord (..),
-    below,
-    cardinal,
-    coordCol,
-    coordLines,
-    coordRow,
-    manhattan,
-  )
+import AOC.Coord (Coord (..), below, cardinal, coordCol, coordLines, manhattan)
 import AOC.Main (simpleMain)
-import AOC.Search (AStep (AStep), astar, dfs)
-import Data.List (find)
+import AOC.Search (AStep (..), astar, dfs)
+import Data.Char (isLetter, ord)
 import Data.Map (Map)
-import Data.Map qualified as M
+import Data.Map qualified as Map
 
-isHallway :: Coord -> Bool
-isHallway c = coordRow c == 1 && not (isRoom c)
+data Cell = Open | Amphi {amphiTarget :: !Int, amphiCost :: !Int}
+  deriving (Eq, Ord, Show)
 
+-- | Compute all the information needed from a character in the input map.
+toCell :: Char -> Maybe Cell
+toCell '.' = Just Open
+toCell a | isLetter a = Just $! Amphi (3 + 2 * (ord a - 65)) (10 ^ (ord a - 65))
+toCell _ = Nothing
+
+-- | Predicate for rooms (rather than hallways)
 isRoom :: Coord -> Bool
 isRoom (C _ c) = c == 3 || c == 5 || c == 7 || c == 9
 
-step :: Map Coord Char -> [AStep (Map Coord Char)]
+-- | Step the simulation once tracking the cost of the move.
+step :: Map Coord Cell -> [AStep (Map Coord Cell)]
 step w =
-  [ AStep w' (manhattan c dest * tokCost tok) 0
-    | (c, tok) <- M.toList w,
-      tok `elem` "ABCD",
+  [ AStep
+      { astepNext = Map.insert c Open (Map.insert dest a w),
+        astepCost = manhattan c dest * stepCost,
+        astepHeuristic = 0
+      }
+    | (c, a@(Amphi target stepCost)) <- Map.toList w,
       dest <- route w c,
       if isRoom c
-        then isHallway dest
+        then
+          not (isRoom dest)
+            && not (roomClean w (coordCol c))
         else
           isRoom dest
-            && coordCol dest == roomCol tok
-            && roomClean w tok
-            && (w M.! below dest) `elem` [tok, '#'],
-      let w' =
-            M.insert c '.' $
-              M.insert dest tok w
+            && coordCol dest == target
+            && roomClean w target
+            && maybe True (a ==) (Map.lookup (below dest) w)
   ]
 
-roomClean :: Map Coord Char -> Char -> Bool
-roomClean w tok =
-  all (`elem` [tok, '.']) $
-    takeWhile ('#' /=) [w M.! C r (roomCol tok) | r <- [2 ..]]
+-- | Check that all the amphis in a room are supposed to be there.
+roomClean :: Map Coord Cell -> Int -> Bool
+roomClean w c = go 2
+  where
+    go r =
+      case Map.lookup (C r c) w of
+        Just Open -> go (r + 1)
+        Just (Amphi t _) -> t == c && go (r + 1)
+        Nothing -> True
 
-done :: Map Coord Char -> Bool
+-- | Check that everything on the map is where it should be.
+done :: Map Coord Cell -> Bool
 done w =
-  all @[]
-    ( \tok ->
-        all (tok ==) $
-          takeWhile ('#' /=) [w M.! C r (roomCol tok) | r <- [2 ..]]
+  all
+    ( \(k, v) ->
+        case v of
+          Open -> True
+          Amphi t _ -> coordCol k == t
     )
-    "ABCD"
+    (Map.toList w)
 
-roomCol :: Char -> Int
-roomCol = \case
-  'A' -> 3
-  'B' -> 5
-  'C' -> 7
-  'D' -> 9
-  _ -> error "invalid token"
-
-tokCost :: Char -> Int
-tokCost = \case
-  'A' -> 1
-  'B' -> 10
-  'C' -> 100
-  'D' -> 1000
-  _ -> error "invalid token"
-
-route :: Map Coord Char -> Coord -> [Coord]
+route :: Map Coord Cell -> Coord -> [Coord]
 route w = dfs move
   where
-    move c = [c' | c' <- cardinal c, w M.! c' == '.']
+    move c = [c' | c' <- cardinal c, Map.lookup c' w == Just Open]
 
 main :: IO ()
 main = simpleMain $ \input ->
-  let inp = M.fromList (coordLines (lines input))
-      Just (_, cost) = find (done . fst) (astar step inp)
-   in (cost, ())
+  let inp = Map.mapMaybe toCell (Map.fromList $ coordLines (lines input))
+   in (head [cost | (w, cost) <- astar step inp, done w], ())
